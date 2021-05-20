cbb6bf6982cd2f49e66fb611341a60a544a56940
Edoardo GIACOMINI <egiacomi@e2r6p12.42.fr>
2021-05-20 17:14:28 +0200

    norme

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,54 +6,55 @@
 /*   By: egiacomi <egiacomi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 15:47:06 by egiacomi          #+#    #+#             */
-/*   Updated: 2021/05/20 16:56:32 by egiacomi         ###   ########.fr       */
+/*   Updated: 2021/05/20 17:14:03 by egiacomi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int     ft_intlen(int n)
+int	ft_intlen(int n)
 {
-    int i;
+	int	i;
 
-    i = 1;
-    if (n < 0)
-        i++;    
-    while (n > 10)
-    {
-        n /= 10;
-        i++;
-    }
-    return (i);
+	i = 1;
+	if (n < 0)
+		i++;
+	while (n > 10)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    char *numalpha;
-    int intlen;
-    int i;
-    
-    intlen = ft_intlen(n);
-    if(!(numalpha = (char *)malloc(sizeof(char) * intlen + 1)))
-        return (NULL);
-    i = 1;
-    if (n < 0)
-    {
-        numalpha[0] = '-';
-        while (intlen-- > 1)
-        {
-            numalpha[intlen] = (n / i) % 10 + '0';
-            i *= 10;
-        }
-    }
-    else
-    {
-        while (intlen-- > 0)
-        {
-            numalpha[intlen] = (n / i) % 10 + '0';
-            i *= 10;
-        }
-    }
-    numalpha[ft_intlen(n) + 1] = '\0';
-    return (numalpha);
+	char	*numalpha;
+	int		intlen;
+	int		i;
+
+	intlen = ft_intlen(n);
+	numalpha = (char *)malloc(sizeof(char) * intlen + 1);
+	if (!numalpha)
+		return (NULL);
+	i = 1;
+	if (n < 0)
+	{
+		numalpha[0] = '-';
+		while (intlen-- > 1)
+		{
+			numalpha[intlen] = (n / i) % 10 + '0';
+			i *= 10;
+		}
+	}
+	else
+	{
+		while (intlen-- > 0)
+		{
+			numalpha[intlen] = (n / i) % 10 + '0';
+			i *= 10;
+		}
+	}
+	numalpha[ft_intlen(n) + 1] = '\0';
+	return (numalpha);
 }
